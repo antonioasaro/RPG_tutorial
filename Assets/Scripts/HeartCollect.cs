4b82053d1cbde2df12a5ba82dbe7c680c178096f
antonioasaro <antonio_asaro@hotmail.com>
2018-02-10 13:52:01 -0500

    Finished episode 004

---
+++ Assets/Scripts/HeartCollect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartCollect : MonoBehaviour {
+
+	public int RotateSpeed;
+	public AudioSource CollectSound;
+	public GameObject ThisHeart;
+
+
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+	
+	// Update is called once per frame
+	void Update () {
+		RotateSpeed = 2;
+		transform.Rotate (0, RotateSpeed, 0, Space.World);
+	}
+
+	void OnTriggerEnter ()
+	{
+		CollectSound.Play ();
+		ThisHeart.SetActive (false);
+	}
+}
